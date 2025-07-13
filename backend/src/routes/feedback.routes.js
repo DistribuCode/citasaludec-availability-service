987d08d26e3fb62d9fd67ff6a48b21e577af6b5b
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-07-13 17:36:55 -0500

    Delete backend directory

--- backend/src/routes/feedback.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/feedback.controller');
-
-router.post('/', controller.createFeedback);
-router.get('/', controller.getAllFeedback);
-
-module.exports = router;
